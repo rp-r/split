ad014d4299759f15edd3665c1a430e165dae1858
mohsinraza <m.raza80@hotmail.com>
2025-12-17 17:01:16 +0100

    add files

--- src/App.tsx
+++ src/App.tsx
@@ -61,8 +61,8 @@ function App() {
   const [personinfo, setPersoninfor] = useState<Friend[]>(initialFriends);
   return (
     <div className='app'>
-      <div className='sidebar'></div>
-      <FriendList />
+      <div className='sidebar'><FriendList /></div>
+
     </div>
   )
 }
@@ -85,12 +85,29 @@ function FriendList() {
 
 function Friend({ friend }: any) {
 
-  return (<>
-    <div>
-      <span>{friend.name}</span></div>
-    <img src={friend.image} alt={friend.name} />
-    <p> you have balance of {friend.balance}</p>
-    <button className='button'>Add</button></>)
+  return (
+    <li> <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+
+      {friend.balance < 0 && (<p className='red'>You own {friend.name} ${Math.abs(friend.balance)}
+
+
+      </p>)}
+
+      {friend.balance > 0 && (<p className='green'>{friend.name}  owns you ${Math.abs(friend.balance)}
+
+
+      </p>)}
+
+
+      {friend.balance === 0 && (<p >You and  {friend.name} are even</p>)}
+
+
+
+
+
+
+      <button className='button'>select</button></li>)
 
 }
 
